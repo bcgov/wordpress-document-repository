e64b08566b93631b5ac79b173f663a0ac098003b
robrien-bcgov <richard.obrien@gov.bc.ca>
2025-12-08 08:19:38 -0800

    remove build files (a side-effect of switching branches to document v1.0 look&feel)

--- build/document-repository/index.asset.php
+++ build/document-repository/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c8d1219fc7ed7ce4d005');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '146d6043a4496eb4ceca');
